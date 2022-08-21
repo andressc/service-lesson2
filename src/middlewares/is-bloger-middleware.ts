10c642b97f9aea662353180da36dc72d2baa1a97
andrejrebrov <674511674511aaqqww>
2022-08-21 17:49:29 +0300

    add services step 1

--- src/middlewares/is-bloger-middleware.ts
+++ src/middlewares/is-bloger-middleware.ts
@@ -1,10 +1,10 @@
 import {Request, Response} from "express";
 import {NextFunction} from "express";
-import {bloggersRepository} from "../repositories/bloggers-repository";
 import {BloggersType} from "../types/bloggersType";
+import {bloggersService} from "../domain/bloggers-service";
 
 export const isBloggerMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const isBlogger: BloggersType | null = await bloggersRepository.findBloggerById(req.body.bloggerId)
+    const isBlogger: BloggersType | null = await bloggersService.findBloggerById(req.body.bloggerId)
 
     if (!isBlogger) {
         return res.status(400).json({ errorsMessages: [{ message: "blogger with this id does not exist", field: "bloggerId" }] });
